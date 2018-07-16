73adb3fa20247d35ad1a749738cb5bd010f52836
Toriumi Ren <arcpace.80@gmail.com>
2018-07-16 13:42:53 +0900

    First creation

---
+++ Env_Extractor.py
@@ -0,0 +1,9 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Env_Extractor.py
+import os
+from datetime import datetime
+
+with open("C:/Script/backup/" + datetime.now().strftime("%Y%m%d_%H%M%S") + "_Env_backup.csv",'w',encoding = "SJIS") as f2: #utf-8_sig
+    for item in os.environ:
+        f2.write(item + "," + os.environ[item] + "\n")
